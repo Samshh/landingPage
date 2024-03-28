5b73db71db7dad0337b39d0a097ad44cd44d3853
Samshh <samdacs2@gmail.com>
2024-03-28 22:08:58 +0800

    lenis fix part 2

--- src/App.vue
+++ src/App.vue
@@ -15,17 +15,13 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Lenis from "@studio-freight/lenis";
 
 gsap.registerPlugin(TextPlugin);
-const lenis = new Lenis({
-  duration: 1.2,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-});
+
+const lenis = new Lenis();
 
 function raf(time) {
   lenis.raf(time);
-  ScrollTrigger.update();
   requestAnimationFrame(raf);
 }
-
 requestAnimationFrame(raf);
 
 onMounted(() => {
